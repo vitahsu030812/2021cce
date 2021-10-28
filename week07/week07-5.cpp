71e6231be644b8951bc1a0939d4da50d42ac10a9
vita hsu <91251449+vitahsu030812@users.noreply.github.com>
2021-10-28 17:40:47 +0800

    Add files via upload

---
+++ week07/week07-5.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=0;i<3;i++)
+    {
+        printf("電腦迴圈的最好寫法:%d\n",i);
+    }
+    for(int i=1;i<=3;i++)
+    {
+        printf("人類數字，從1數:%d\n",i);
+    }
+    for(int i=0;i<=3;i++)
+    {
+        printf("不好的寫法:%d\n",i);
+    }
+}
